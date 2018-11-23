6405a32627271b7d6177a5e41266dafc6f9aeb6a
David Weiss <davidweisss@gmail.com>
2018-11-23 04:07:09 +0000

    working proto

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var fs = require('fs');
-var key = fs.readFileSync('/etc/letsencrypt/live/iHodL.rocks/privkey.pem');
-var cert = fs.readFileSync('/etc/letsencrypt/live/iHodL.rocks/fullchain.pem')
-var https = require('https');
+var key = fs.readFileSync('/etc/letsencrypt/live/ihodl.rocks/privkey.pem');
+var cert = fs.readFileSync('/etc/letsencrypt/live/ihodl.rocks/fullchain.pem')
+var https = require('https')
 var http = require('http');
 var https_options = {
 	key: key,
@@ -12,9 +12,9 @@ var PORT = 443;
 var HOST = '0.0.0.0';
 app = express();
 app.get('/', function (req, res) {
-	res.sendFile('/home/davidweisss/Belbit/app/build/index.html')
+	res.sendFile('/home/davidweisss/iHodLWeb/app/index.html')
 })
-app.use(express.static('/home/davidweisss/Belbit/app/build'));
+app.use(express.static('/home/davidweisss/iHodLWeb/app/index.html'));
 server = https.createServer(https_options, app).listen(PORT, HOST);
 console.log('HTTPS Server listening on %s:%s', HOST, PORT);
 
